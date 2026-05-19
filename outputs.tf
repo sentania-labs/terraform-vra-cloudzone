cc644df1fb10ad15cb4676c23c128d7b063cea26
Scott R Bowe <scottb@sentania.net>
2026-05-19 17:32:31 -0500

    fix: add output description (#6)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "cloud_zone" {
+  description = "The cloud zone name, ID, and external region ID"
   value = {
     "name"               = vra_zone.this.name,
     "id"                 = vra_zone.this.id,
